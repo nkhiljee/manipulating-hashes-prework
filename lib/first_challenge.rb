e83143522f4cd1f8e717537664130edbb728ca6d
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-16 07:26:35 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,7 @@ def first_challenge
   }
 
   #your code here
+contacts.each do |person, data|
 contacts["Freddy Mercury"][:favorite_icecream_flavors].remove ("strawberry")
 
   #remember to return your newly altered contacts hash!
